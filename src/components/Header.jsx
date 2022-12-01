13d391e1c3d875738f7ef88f1322bbef515d709b
Alexis <ajeria@outlook.es>
2022-12-01 16:05:31 -0300

    primer commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+//Componente header
+
+function Header() {
+  return (
+    <div>
+      <img
+        src="https://nerdmacia.cl/wp-content/uploads/2017/06/Rick_and_Morty_Logo_and_Image.png"
+        alt=""
+        className="header"
+      />
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/components/Landing.jsx
@@ -0,0 +1,17 @@
+//componente pagina inicio
+
+function Landing() {
+  return (
+    <div className="landing ">
+      <h1>Bienvenidos</h1>
+      <h2>En este sitio encontraras todos los personajes de Rick and Morty, locaciones y más</h2>
+      <img
+          id="imagen"
+          src="https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/styles/950/public/media/image/2019/05/rick-morty-temporada-4-llegara-noviembre.jpg?itok=6Bguw2Cu"
+          alt=""
+        ></img>
+    </div>
+  );
+}
+
+export default Landing;
